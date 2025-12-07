d5cc6edbd28c50d57775aac6dde8db5d33f7664c
amrh20 <amrhamdy058@gmail.com>
2025-12-07 23:30:38 +0200

    new ui

--- lib/core/widgets/animated_background.dart
+++ lib/core/widgets/animated_background.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
-import 'package:wish_listy/core/constants/app_colors.dart';
 
 class AnimatedBackground extends StatefulWidget {
   final Widget? child;
